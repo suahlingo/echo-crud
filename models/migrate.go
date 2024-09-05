00e8f18a8f3c769b0f8f17d042acd53add9e7c76
suahlingo <sgua0720@gmail.com>
2024-09-05 12:45:14 +0900

    feat : refresh migrate delete

--- models/migrate.go
+++ models/migrate.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"encoding/json"
 	"gorm.io/gorm"
 )
 
@@ -12,25 +11,6 @@ type User struct {
 	Age   int    `gorm:"not null" json:"age"`
 }
 
-// RefreshArr 구조체 (배열을 JSON으로 저장)
-type RefreshArr struct {
-	ID      int     `gorm:"primaryKey;autoIncrement" json:"id"`
-	Arr     [20]int `gorm:"-" json:"arr"` // 배열 필드
-	ArrJSON []byte  `gorm:"type:json" json:"-"`
-}
-
-// BeforeCreate: 배열을 JSON으로 변환해 저장
-func (r *RefreshArr) BeforeCreate(tx *gorm.DB) (err error) {
-	r.ArrJSON, err = json.Marshal(r.Arr)
-	return
-}
-
-// AfterFind: JSON을 다시 배열로 변환
-func (r *RefreshArr) AfterFind(tx *gorm.DB) (err error) {
-	err = json.Unmarshal(r.ArrJSON, &r.Arr)
-	return
-}
-
 func Migrate(db *gorm.DB) error {
-	return db.AutoMigrate(&User{}, &RefreshArr{})
+	return db.AutoMigrate(&User{})
 }
